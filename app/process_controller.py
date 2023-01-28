9044c6156767d39e0e811bbbd9cd8d850d5e5b16
leonardorodrigues42 <leonardorodriguesaai@outlook.com>
2023-01-28 18:10:27 -0300

    feat: implementando rotas e controller

---
+++ app/process_controller.py
@@ -0,0 +1,80 @@
+from sqlalchemy_get_or_create import get_or_create
+from .models.transaction import Transaction
+from .models.recipient import Recipient
+from .models.type import Type
+from sqlalchemy import func
+
+
+class ProcessController():    
+    def __init__(self, db, line):
+        self.db    = db
+        self.line  = line
+        self.type  = line[0]
+        self.date  = line[1:9]
+        self.cpf   = line[19:30]
+        self.card  = line[30:42]
+        self.hour  = line[42:48]
+        self.owner = line[48:62]
+        self.name  = line[62:81]
+        self.value = int(line[9:19]) / 100
+    
+        
+    def return_type(self):
+        type_obj = Type.query.filter_by(number=int(self.type)).first()
+        
+        return type_obj
+    
+    
+    def recipient_handle(self):
+        recipient = get_or_create(
+            self.db.session,
+            Recipient, 
+            owner=self.owner,
+            cpf=self.cpf,
+            name=self.name
+        )
+                    
+        recipient_obj = Recipient.query.filter_by(cpf=self.cpf, name=self.name).first()
+        
+        return recipient_obj
+    
+    
+    def process_transaction(self):
+        type_obj = self.return_type()
+        recipient_obj = self.recipient_handle()      
+        
+        transaction = Transaction(
+            type_id=type_obj.id,
+            date=self.date,
+            value=self.value,
+            card=self.value,
+            hour=self.hour,
+            recipient_id=recipient_obj.id,
+        )
+        
+        self.db.session.add(transaction)
+        self.db.session.commit()
+        
+        transaction_obj = Transaction.query.filter_by(
+            hour=transaction.hour,
+            date=transaction.date
+        ).first()
+        
+        return transaction_obj
+    
+    @classmethod
+    def transactions_by_recipient(cls, db):
+        recipients = Recipient.query.all()
+        
+        filtered_transactions = dict()
+        
+        for recipient in recipients:
+            transactions = []
+            balance = recipient.balance()
+            for transaction in recipient.transactions:
+                transactions.append(transaction.to_dict())
+            
+            filtered_transactions[recipient.name] = {"transactions": transactions, **recipient.balance()}
+            
+                
+        return filtered_transactions
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,55 @@
-from flask import Flask, jsonify, request
-from .models.transaction import Transaction
-from .models.type import Type
-from .models.recipient import Recipient
+from flask import Flask, jsonify, request, render_template
+from .process_controller import ProcessController
+from .utils import allowed_file
+from .database import db
+import asyncio
+
 
 def init_app(app):
+    @app.route('/upload')
+    def upload():
+        return render_template("upload.html")
+    
+    
+    @app.route('/upload', methods=['POST'])
+    def upload_file():
+        
+        if 'file_upload' not in request.files:
+            return 'No file part'
+
+        file = request.files['file_upload']
+
+        if file.filename == '':
+            return 'No selected file'
+
+        if file and allowed_file(file.filename):
+            file_content = file.read()
+            lines = file_content.splitlines()
+            
+            db.session.autoflush = False
+            
+            transactions = []
+            
+            for line in lines:
+                line  = line.decode()
+                if line:
+                    transaction_obj = ProcessController(db, line).process_transaction()                                    
+                    transactions.append(transaction_obj.to_dict())
+            return jsonify(transactions), 201
+
+        return 'Invalid file type'
+    
+
+    @app.route("/transactions")
+    def transactions_list():
+        transactions = ProcessController.transactions_by_recipient(db)    
+
+        return jsonify(transactions), 201
+    
+    
     @app.get('/')
     def home():
         tipos = {"message": "funfou aqui"}
 
         return tipos, 200
+    
